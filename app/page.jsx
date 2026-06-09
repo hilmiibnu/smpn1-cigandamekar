ab492a24b7046684a81953145d4d3dda8e30caf8
hilmiibnu <hilmiibnu96@gmail.com>
2026-06-09 13:11:38 +0700

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,9 @@
-import Navbar from "@/components/Navbar"
+
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-gray-100">
-      <Navbar />
+   
 
       <div className="p-8">
 
